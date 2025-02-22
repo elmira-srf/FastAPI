27c212ddf40ac3d6468d689a553df27ae1cf1a8a
Elmira <elmira.sarrafi@gmail.com>
2025-02-22 18:35:25 -0500

    daily projects

---
+++ Daily_P_Proj/code_dictation.py
@@ -0,0 +1,10 @@
+numbers = [3, 5, 6]
+product = numbers[0]*numbers[1]*numbers[2]
+if product%2 == 0 :
+    product_status = "Even"
+else:
+    product_status = "Odd"
+
+print(product)
+print(product_status)
+
---
+++ Daily_P_Proj/movie_night.py
@@ -0,0 +1,9 @@
+movies = {
+    "Action": ["Mad Max: Fury Road", "John Wick", "Die Hard", "Gladiator"],
+    "Comedy": ["Superbad", "Step Brothers", "The Big Lebowski", "Dumb and Dumber"],
+    "Drama": ["Forrest Gump", "The Shawshank Redemption", "Titanic", "The Green Mile"],
+    "Sci-Fi": ["Interstellar", "Inception", "The Matrix", "Blade Runner 2049"],
+    "Horror": ["The Conjuring", "A Nightmare on Elm Street", "Get Out", "The Exorcist"]
+}
+
+genre = input()
